a3d3a5a879ec78683eb92e22eb0147226735ee5e
Graffias44 <kaochan44.us3126@gmail.com>
2021-04-17 21:27:41 +0900

    詳細表示機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,32 @@
 class PostsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
+  # 略
 
   def new
+  # ***** 以下を追加 *****
+    @post = Post.new
+  # ***** 以上を追加 *****
   end
 
   def create
+  # ***** 以下を追加 *****
+    post = Post.create!(post_params)
+    redirect_to post
+  # ***** 以上を追加 *****
   end
 
-  def edit
+  def show
+    # ***** 以下を追加 *****
+    @post = Post.find(params[:id])
+    # ***** 以上を追加 *****
   end
 
-  def update
-  end
+  # 略
 
-  def destroy
+  # ***** 以下を追加 *****
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
+  # ***** 以上を追加 *****
 end
